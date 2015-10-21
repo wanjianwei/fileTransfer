b1933364ac15c4c1069adc86762133cae06848ad
jway <1570846772@qq.com>
2015-10-21 08:31:42 +0800

    Initial Commit

---
+++ fileTransfer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  fileTransfer
+//
+//  Created by jway on 15/10/21.
+//  Copyright © 2015年 jway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
